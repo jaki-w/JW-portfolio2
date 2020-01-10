7e6e87f76fc1a5146ed854d02217180540b701ba
Jaki Widby <jaki.widby@gmail.com>
2020-01-10 13:58:50 -0800

    create js functionality for additional buttons

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
   $("button#about").click(function() {
     $(".about-hidden").toggle();
   });
+  $("button#projects").click(function() {
+    $(".myWork-hidden").toggle();
+  });
+  $("button#languages").click(function() {
+    $(".languages-hidden").toggle();
+  });
 });
